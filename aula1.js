c020abe41610b7186366ea5628b8569e1be0b8fd
André Luiz <andrebusiness42@gmail.com>
2023-10-23 21:22:45 -0300

    Fist commit

---
+++ aula1.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+let nome="André"
+let sobrenome="Luz"
+
+let nomeCompleto=nome+" "+sobrenome;
+console.log("nome:" + nomeCompleto)
+
+let carro="gol"
+let modelo="volkswagen"
+
+let marcaseModelo=carro+' 'modelo;
+console.log("carro:" + modelodecarro)
